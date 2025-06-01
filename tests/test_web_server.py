d5d1c7644c17ca7bc01a0b1aeee387b680b127f2
Andrew Spratley <aps831@yahoo.co.uk>
2025-06-01 18:06:36 +0100

    feat: test ci

--- tests/test_web_server.py
+++ tests/test_web_server.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import httpx
+import time
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 
@@ -8,6 +9,9 @@ def image_name():
   return os.environ.get("IMAGE_UNDER_TEST_NAME", "registry.local:5000/workflows-testbed-python/testbed-python:latest")
 
 server = DockerContainer(image_name()).with_exposed_ports(8000)
+
+def server_address():
+  return 'http://' + server.get_container_host_ip() + ':' + server.get_exposed_port(8000)
 
 @pytest.fixture(scope="module", autouse=True)
 def setup(request):
@@ -25,7 +29,7 @@ def test_connection():
     # Given
 
     # When
-    r = httpx.get('http://localhost:' + server.get_exposed_port(8000))
+    r = httpx.get(server_address())
 
     # Then
     assert r.status_code == 200
